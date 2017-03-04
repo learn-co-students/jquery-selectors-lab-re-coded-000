73faab81365b9314f22ea9512e0f8100db10000b
Ali Diyab <alidiyab1997@gmail.com>
2017-03-04 15:42:20 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 'use-strict';
+
+function h1Selector() {
+  var h1 = $('h1')
+  return h1;
+}
+
+function liInOlSelector() {
+  var LiOl = $('ol li')
+  return LiOl;
+}
+
+function linkSelector() {
+  var link = $("div #box4 a")
+  return link;
+}
+
+function imageSelector() {
+  var img = $("img[alt='cat sleeping']")
+  return img;
+}
+
+function checkboxInputSelector() {
+  var check = $("input[type='checkbox']")
+  return check;
+}
