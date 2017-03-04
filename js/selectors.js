9693264e19bc155596b48bbb9817dd520c850caa
Muataz Aziz <muataz.aziz91@yahoo.com>
2017-03-04 13:25:40 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 'use-strict';
+
+function h1Selector(){
+  return $('h1');
+}
+
+function liInOlSelector(){
+  return $('ol li');
+}
+
+function linkSelector(){
+  return $('#box4 a , .box5 a');
+  // return $('#box4.box5 a'); also works like the line above
+}
+
+function imageSelector(){
+  return $('img[alt="cat sleeping"]');
+}
+
+function checkboxInputSelector(){
+  return $('input[type="checkbox"]');
+}
